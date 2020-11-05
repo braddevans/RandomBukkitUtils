3ba2dd3e7b24993c59dc00e7557645d68708cf2f
bradd evans <braddevans123@gmail.com>
2020-11-05 10:34:32 +0000

    Add Documentation to sign util

--- src/main/java/uk/co/breadhub/randombukkitutils/world/SignUtils.java
+++ src/main/java/uk/co/breadhub/randombukkitutils/world/SignUtils.java
@@ -7,6 +7,28 @@ import uk.co.breadhub.randombukkitutils.api.world.SignUtilsApi;
 
 public class SignUtils implements SignUtilsApi {
 
+    /**
+     * <h1>Update sign</h1>
+     *
+     * <p>Example code 1</p>
+     * <code>
+     *     @EventHandler
+     *     public void onSignPlace(final SignChangeEvent event) {
+     *         final Player player = event.getPlayer();
+     *         final String[] lines = event.getLines();
+     *         updateSign(event, lines);
+     *     }
+     * </code>
+     *
+     * <p>Example code 2</p>
+     * <code>
+     *     // the code below will create the sign event and call it to trigger the below function
+     *     updateSign(new SignChangeEvent(location.getBlock(), sender, lines), lines)
+     * </code>
+     *
+     * @param event
+     * @param lines
+     */
     @Override
     public void updateSign(SignChangeEvent event, final String[] lines) {
         event.setLine(0, String.valueOf(lines[0]));
@@ -15,6 +37,15 @@ public class SignUtils implements SignUtilsApi {
         event.setLine(3, String.valueOf(lines[3]));
     }
 
+    /**
+     * <h1>Remove sign at Location</h1>
+     * <p>
+     *     Location: world x y z location;
+     *     Boolean: will the block drop its item
+     * </p>
+     * @param loc
+     * @param DropsItem
+     */
     @Override
     public void removeSignAt(Location loc, boolean DropsItem) {
         if (DropsItem) {
